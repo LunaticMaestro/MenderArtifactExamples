b7e26019b14f01ef426b2de5e21ff1602239b92a
Deepak Sahu <deepak@iiitkalyani.ac.in>
2021-02-27 06:34:28 +0530

    Create a streamlit app

---
+++ ApplicationUpdate/StreamlitApp/st_web_interface_v1/st_web_interface/README.md
@@ -0,0 +1,7 @@
+# v1.0
+Creates a streamlit's web interface with simple title and text.
+
+```
+	pip install -r requirements.txt
+	streamlit run st_web_interface/__main__.py
+```
---
+++ ApplicationUpdate/StreamlitApp/st_web_interface_v1/st_web_interface/st_web_interface/__main__.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+	st.title("Internship Assignment")
+	st.write("Hi there employee, we will change this page with\
+			next update of the app using mender. \
+			")
+
+if __name__=='__main__':
+	main()
